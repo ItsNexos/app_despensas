10d987a6485dd73de06cb771273702840cd581d8
Hugo <hugoherreramontecino@gmail.com>
2024-10-16 21:15:07 -0300

    login

--- lib/features/user_auth/presentation/pages/sign_up_page.dart
+++ lib/features/user_auth/presentation/pages/sign_up_page.dart
@@ -1,87 +1,90 @@
-import 'package:app_despensas/features/user_auth/presentation/pages/login_page.dart';
-import 'package:app_despensas/features/user_auth/presentation/widgets/form_container_widget.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'login_page.dart';
 
-class SignUpPage extends StatelessWidget {
-  const SignUpPage({super.key});
+class SignUpPage extends StatefulWidget {
+  @override
+  _SignUpPageState createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  TextEditingController _nameController = TextEditingController();
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _signUp() async {
+    String name = _nameController.text;
+    String email = _emailController.text;
+    String password = _passwordController.text;
+
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password);
+
+      await userCredential.user?.updateDisplayName(name);
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => LoginPage()));
+    } catch (e) {
+      print('Error al registrar el usuario: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "Registro de usuario",
-            style: TextStyle(color: Colors.white),
-          ),
-          backgroundColor: Colors.blue,
-        ),
-        body: Center(
-            child: Padding(
+      appBar: AppBar(
+        title: Text("Registro de Usuario"),
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 15),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
                 "Registro",
-                style: TextStyle(
-                  fontSize: 27,
-                  fontWeight: FontWeight.bold,
-                ),
+                style: TextStyle(fontSize: 27, fontWeight: FontWeight.bold),
               ),
-              SizedBox(
-                height: 30,
+              SizedBox(height: 30),
+              TextField(
+                controller: _nameController,
+                decoration: InputDecoration(hintText: "Nombre"),
               ),
-              FormContainerWidget(
-                hintText: "Nombre de usuario",
-                isPasswordField: false,
+              SizedBox(height: 20),
+              TextField(
+                controller: _emailController,
+                decoration: InputDecoration(hintText: "Correo"),
               ),
-              SizedBox(
-                height: 20,
+              SizedBox(height: 20),
+              TextField(
+                controller: _passwordController,
+                decoration: InputDecoration(hintText: "Contraseña"),
+                obscureText: true,
               ),
-              FormContainerWidget(
-                hintText: "Email",
-                isPasswordField: false,
+              SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: _signUp,
+                child: Text("Registrarse"),
               ),
-              SizedBox(
-                height: 20,
-              ),
-              FormContainerWidget(
-                hintText: "Contraseña",
-                isPasswordField: true,
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Container(
-                  width: double.infinity,
-                  height: 45,
-                  decoration: BoxDecoration(
-                    color: Colors.blue,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Center(
-                      child: Text(
-                    "Registrarse",
-                    style: TextStyle(
-                        color: Colors.white, fontWeight: FontWeight.bold),
-                  ))),
-              SizedBox(
-                height: 20,
-              ),
+              SizedBox(height: 20),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text("Ya tienes una cuenta?"),
-                  SizedBox(
-                    width: 5,
-                  ),
+                  Text("¿Ya tienes una cuenta?"),
+                  SizedBox(width: 5),
                   GestureDetector(
                     onTap: () {
-                      Navigator.pushAndRemoveUntil(
-                        context,
-                        MaterialPageRoute(builder: (context) => LoginPage()),
-                        (route) => false,
-                      );
+                      Navigator.pushReplacement(context,
+                          MaterialPageRoute(builder: (context) => LoginPage()));
                     },
                     child: Text(
                       "Iniciar sesión",
@@ -95,6 +98,8 @@ class SignUpPage extends StatelessWidget {
               ),
             ],
           ),
-        )));
+        ),
+      ),
+    );
   }
 }
