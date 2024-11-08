9b8b296fad58e68628752f75125316c204cafdb8
Hugo <hugoherreramontecino@gmail.com>
2024-11-08 00:58:23 -0300

    recetas first version

---
+++ lib/pages/recipes_add.dart
@@ -0,0 +1,327 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class RecipeAddPage extends StatefulWidget {
+  final String user;
+
+  const RecipeAddPage({Key? key, required this.user}) : super(key: key);
+
+  @override
+  _RecipeAddPageState createState() => _RecipeAddPageState();
+}
+
+class _RecipeAddPageState extends State<RecipeAddPage> {
+  bool _publishForOthers = false;
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController _titleController = TextEditingController();
+  TextEditingController _preparationController = TextEditingController();
+  TextEditingController _estimatedTimeController = TextEditingController();
+
+  List<Map<String, dynamic>> ingredients = [];
+  TextEditingController _ingredientNameController = TextEditingController();
+  TextEditingController _ingredientQuantityController = TextEditingController();
+  String _ingredientUnit = 'unidades';
+
+  List<String> userProducts = []; // Lista de productos del usuario
+  List<String> filteredSuggestions = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadUserProducts();
+  }
+
+  // Método para cargar productos de la despensa del usuario
+  Future<void> _loadUserProducts() async {
+    final despensasRef = FirebaseFirestore.instance
+        .collection('usuarios')
+        .doc(widget.user)
+        .collection('despensas');
+
+    final despensasSnapshot = await despensasRef.get();
+    List<String> loadedProducts = [];
+
+    for (var despensaDoc in despensasSnapshot.docs) {
+      final productosSnapshot =
+          await despensaDoc.reference.collection('productos').get();
+      for (var productDoc in productosSnapshot.docs) {
+        loadedProducts.add(productDoc['nombre']);
+      }
+    }
+
+    setState(() {
+      userProducts = loadedProducts.toSet().toList(); // Evitar duplicados
+    });
+  }
+
+  // Filtra las sugerencias de acuerdo con lo que el usuario escribe
+  void _filterSuggestions(String query) {
+    setState(() {
+      filteredSuggestions = userProducts
+          .where(
+              (product) => product.toLowerCase().contains(query.toLowerCase()))
+          .toList();
+    });
+  }
+
+  // Agregar un ingrediente a la receta
+  void _addIngredient() {
+    if (_ingredientNameController.text.isNotEmpty &&
+        _ingredientQuantityController.text.isNotEmpty) {
+      setState(() {
+        // Si el ingrediente no está en las sugerencias, lo agregamos como nuevo
+        String ingredientName = _ingredientNameController.text;
+        if (!userProducts.contains(ingredientName)) {
+          userProducts.add(
+              ingredientName); // Lo añadimos a la lista de productos del usuario
+        }
+
+        ingredients.add({
+          'nombre': ingredientName,
+          'cantidad': int.parse(_ingredientQuantityController.text),
+          'medida': _ingredientUnit,
+        });
+        _ingredientNameController.clear();
+        _ingredientQuantityController.clear();
+        _ingredientUnit = 'unidades';
+      });
+    }
+  }
+
+  // Guardar la receta en Firestore
+  Future<void> _saveRecipe() async {
+    if (_formKey.currentState!.validate()) {
+      final recipeData = {
+        'titulo': _titleController.text,
+        'preparacion': _preparationController.text,
+        'categorias': selectedCategories,
+        'tiempoEstimado': int.tryParse(_estimatedTimeController.text) ?? 0,
+      };
+
+      final recipeRef = await FirebaseFirestore.instance
+          .collection('usuarios')
+          .doc(widget.user)
+          .collection('recetas')
+          .add(recipeData);
+
+      for (var ingredient in ingredients) {
+        await recipeRef.collection('ingredientes').add(ingredient);
+      }
+
+      // Si la casilla de "Publicar receta para otros usuarios" está marcada, guardamos la receta en la colección pública
+      if (_publishForOthers) {
+        final publicRecipeRef =
+            FirebaseFirestore.instance.collection('recetas').doc(recipeRef.id);
+        await publicRecipeRef.set(recipeData);
+        for (var ingredient in ingredients) {
+          await publicRecipeRef.collection('ingredientes').add(ingredient);
+        }
+      }
+
+      Navigator.of(context).pop();
+    }
+  }
+
+  List<String> selectedCategories = [];
+  final List<String> categories = [
+    'Entrada',
+    'Plato de fondo',
+    'Ensalada',
+    'Repostería',
+    'Bebestible',
+    'Vegetariano',
+    'Carnes',
+    'Vegano'
+  ];
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _preparationController.dispose();
+    _ingredientQuantityController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Añadir Receta"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            children: [
+              TextFormField(
+                controller: _titleController,
+                decoration:
+                    const InputDecoration(labelText: "Título de la receta"),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Por favor ingresa un título";
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              TextFormField(
+                controller: _preparationController,
+                decoration: const InputDecoration(labelText: "Preparación"),
+                maxLines: 4,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Por favor ingresa la preparación";
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              TextFormField(
+                controller: _estimatedTimeController,
+                decoration: const InputDecoration(
+                  labelText: "Tiempo estimado (minutos)",
+                ),
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Por favor ingresa el tiempo estimado";
+                  }
+                  if (int.tryParse(value) == null) {
+                    return "Por favor ingresa un número válido";
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              Text("Ingredientes:",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+              ListView.builder(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemCount: ingredients.length,
+                itemBuilder: (context, index) {
+                  final ingredient = ingredients[index];
+                  return ListTile(
+                    title: Text(
+                        "${ingredient['cantidad']} ${ingredient['medida']} de ${ingredient['nombre']}"),
+                    trailing: IconButton(
+                      icon: Icon(Icons.delete),
+                      onPressed: () {
+                        setState(() {
+                          ingredients.removeAt(index);
+                        });
+                      },
+                    ),
+                  );
+                },
+              ),
+              // Campo para seleccionar ingredientes con sugerencias
+              TextFormField(
+                controller: _ingredientNameController,
+                decoration: const InputDecoration(
+                  labelText: "Nombre del ingrediente",
+                ),
+                onChanged:
+                    _filterSuggestions, // Filtra las sugerencias a medida que se escribe
+              ),
+              // Muestra las sugerencias filtradas debajo del campo de texto
+              if (_ingredientNameController.text.isNotEmpty)
+                ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: filteredSuggestions.length,
+                  itemBuilder: (context, index) {
+                    final suggestion = filteredSuggestions[index];
+                    return ListTile(
+                      title: Text(suggestion),
+                      onTap: () {
+                        _ingredientNameController.text = suggestion;
+                        setState(() {
+                          filteredSuggestions = [];
+                        });
+                      },
+                    );
+                  },
+                ),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      controller: _ingredientQuantityController,
+                      decoration: const InputDecoration(labelText: "Cantidad"),
+                      keyboardType: TextInputType.number,
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  DropdownButton<String>(
+                    value: _ingredientUnit,
+                    items: [
+                      'unidades',
+                      'g',
+                      'kg',
+                      'lt',
+                      'ml',
+                      "tazas",
+                      "cucharadas"
+                    ].map((String unit) {
+                      return DropdownMenuItem<String>(
+                        value: unit,
+                        child: Text(unit),
+                      );
+                    }).toList(),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        _ingredientUnit = newValue!;
+                      });
+                    },
+                  ),
+                ],
+              ),
+              ElevatedButton(
+                onPressed: _addIngredient,
+                child: const Text("Agregar ingrediente"),
+              ),
+              const SizedBox(height: 20),
+              Text("Categorías:",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+              Wrap(
+                spacing: 8.0,
+                children: categories.map((category) {
+                  return FilterChip(
+                    label: Text(category),
+                    selected: selectedCategories.contains(category),
+                    onSelected: (isSelected) {
+                      setState(() {
+                        if (isSelected) {
+                          selectedCategories.add(category);
+                        } else {
+                          selectedCategories.remove(category);
+                        }
+                      });
+                    },
+                  );
+                }).toList(),
+              ),
+              const SizedBox(height: 20),
+              CheckboxListTile(
+                title: const Text("Publicar receta para otros usuarios"),
+                value: _publishForOthers,
+                onChanged: (bool? value) {
+                  setState(() {
+                    _publishForOthers = value ?? false;
+                  });
+                },
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: _saveRecipe,
+                child: const Text("Guardar receta"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/recipes_page.dart
+++ lib/pages/recipes_page.dart
@@ -1,7 +1,212 @@
+import 'package:app_despensas/pages/recipes_add.dart';
+import 'package:app_despensas/pages/public_recipes.dart'; // Importa la nueva página PublicRecipesPage
+import 'package:app_despensas/pages/recipes_view.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class RecipesPage extends StatelessWidget {
-  const RecipesPage({super.key});
+class RecipesPage extends StatefulWidget {
+  const RecipesPage({Key? key}) : super(key: key);
+
+  @override
+  _RecipesPageState createState() => _RecipesPageState();
+}
+
+class _RecipesPageState extends State<RecipesPage>
+    with SingleTickerProviderStateMixin {
+  final User? user = FirebaseAuth.instance.currentUser;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Future<List<Map<String, dynamic>>> _getRecipesWithMatches() async {
+    final recipesRef = FirebaseFirestore.instance
+        .collection('usuarios')
+        .doc(user!.uid)
+        .collection('recetas');
+
+    final despensasRef = FirebaseFirestore.instance
+        .collection('usuarios')
+        .doc(user!.uid)
+        .collection('despensas');
+
+    final despensasSnapshot = await despensasRef.get();
+    List<String> userProducts = [];
+    for (var despensa in despensasSnapshot.docs) {
+      final productosSnapshot =
+          await despensa.reference.collection('productos').get();
+      userProducts
+          .addAll(productosSnapshot.docs.map((doc) => doc['nombre'] as String));
+    }
+
+    final recipesSnapshot = await recipesRef.get();
+    List<Map<String, dynamic>> recipesWithMatches = await Future.wait(
+      recipesSnapshot.docs.map((recipeDoc) async {
+        final recipeData = recipeDoc.data();
+        final ingredientesSnapshot =
+            await recipeDoc.reference.collection('ingredientes').get();
+        final ingredientes = ingredientesSnapshot.docs
+            .map((doc) => doc['nombre'] as String)
+            .toList();
+
+        final matchCount = ingredientes
+            .where((ingredient) => userProducts.contains(ingredient))
+            .length;
+
+        return {
+          'id': recipeDoc.id,
+          'titulo': recipeData['titulo'],
+          'tiempoEstimado': recipeData['tiempoEstimado'],
+          'preparacion': recipeData['preparacion'],
+          'matchCount': matchCount,
+        };
+      }).toList(),
+    );
+
+    recipesWithMatches
+        .sort((a, b) => b['matchCount'].compareTo(a['matchCount']));
+    return recipesWithMatches;
+  }
+
+  void _showDeleteConfirmationDialog(String recipeId) {
+    bool deleteForOthers = false;
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Confirmar eliminación"),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text("¿Estás seguro de que deseas eliminar esta receta?"),
+              Row(
+                children: [
+                  Checkbox(
+                    value: deleteForOthers,
+                    onChanged: (bool? value) {
+                      setState(() {
+                        deleteForOthers = value ?? false;
+                      });
+                    },
+                  ),
+                  const Text("Eliminar para todos"),
+                ],
+              ),
+            ],
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Cierra el cuadro de diálogo
+              },
+              child: const Text("Cancelar"),
+            ),
+            TextButton(
+              onPressed: () {
+                // Eliminar la receta de la colección del usuario
+                FirebaseFirestore.instance
+                    .collection('usuarios')
+                    .doc(user!.uid)
+                    .collection('recetas')
+                    .doc(recipeId)
+                    .delete();
+
+                // Eliminar también de la colección pública si se selecciona la opción
+                if (deleteForOthers) {
+                  FirebaseFirestore.instance
+                      .collection('recetas')
+                      .doc(recipeId)
+                      .delete();
+                }
+                Navigator.pop(context);
+                // Recargar la lista de recetas
+                setState(() {
+                  _getRecipesWithMatches(); // Actualiza la lista llamando a la función de nuevo
+                });
+              },
+              child: const Text("Eliminar"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget _buildRecipeList() {
+    return FutureBuilder<List<Map<String, dynamic>>>(
+      future: _getRecipesWithMatches(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return const Center(child: CircularProgressIndicator());
+        }
+
+        final recipes = snapshot.data!;
+        return ListView.builder(
+          itemCount: recipes.length,
+          itemBuilder: (context, index) {
+            final recipe = recipes[index];
+            return Card(
+              elevation: 4,
+              margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12)),
+              child: ListTile(
+                contentPadding: const EdgeInsets.all(16),
+                title: Text(
+                  recipe['titulo'],
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 8),
+                    Text(
+                      "Tiempo estimado: ${recipe['tiempoEstimado']} min",
+                      style: const TextStyle(fontSize: 16),
+                    ),
+                    const SizedBox(height: 4),
+                    Text(
+                      "Ingredientes: ${recipe['matchCount']} coincidencias",
+                      style: const TextStyle(fontSize: 16, color: Colors.grey),
+                    ),
+                  ],
+                ),
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.delete, color: Colors.red),
+                      onPressed: () {
+                        _showDeleteConfirmationDialog(recipe['id']);
+                      },
+                    ),
+                    const Icon(Icons.arrow_forward_ios),
+                  ],
+                ),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => RecipeViewPage(
+                        recipeId: recipe['id'],
+                        user: user!,
+                      ),
+                    ),
+                  );
+                },
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,11 +215,48 @@ class RecipesPage extends StatelessWidget {
         title: const Text('Recetas'),
         backgroundColor: const Color(0xFFB0C4DE),
       ),
-      body: const Center(
-        child: Text(
-          'Estás en la página de recetas',
-          style: TextStyle(fontSize: 24),
-        ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  'Mis Recetas',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const PublicRecipesPage(),
+                      ),
+                    );
+                  },
+                  child: const Text('Explorar recetas'),
+                ),
+              ],
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => RecipeAddPage(
+                    user: user!.uid,
+                  ),
+                ),
+              );
+            },
+            child: const Text('Agregar receta'),
+          ),
+          Expanded(
+            child: _buildRecipeList(),
+          ),
+        ],
       ),
     );
   }
